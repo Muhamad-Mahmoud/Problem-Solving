d23ef3980587adad1f12556ec021535bfea10a45
Muhamad-Mahmoud <149812732+Muhamad-Mahmoud@users.noreply.github.com>
2024-01-27 14:13:27 +0200

    Update Y.cpp

--- Sheet #2 (Loops)/Y.cpp
+++ Sheet #2 (Loops)/Y.cpp
@@ -3,15 +3,16 @@
 using namespace std;
 int main()
 {
-	int N;
-	cin >> N;
-	for(int i =1;i<= N;i++) {
-		int n1 = 0, n2 = 1, temp;
-		temp = n1 + n2; // 1 // 2
-		n1 = n2; // 1 // 1
-		n2 = temp; // 1 // 2
-	}
+  int N;
+  cin >> N;
+  int n1 = 0, n2 = 1;
+  for (int i = 1; i <= N; i++)
+  {
+    cout << n1 << " ";
+    int temp = n1 + n2;
+    n1 = n2;
+    n2 = temp;
+  }
 
-
-	return 0;
+  return 0;
 }
