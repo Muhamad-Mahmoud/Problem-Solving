23bcea830b122b56fce4378210e4342dbe34761f
Muhamad-Mahmoud <149812732+Muhamad-Mahmoud@users.noreply.github.com>
2024-01-27 14:13:49 +0200

    Update Z.cpp

--- Sheet #2 (Loops)/Z.cpp
+++ Sheet #2 (Loops)/Z.cpp
@@ -1,9 +1,22 @@
-// 
+// Z. Three Numbers
+
 #include <iostream>
-#include<iomanip>
 using namespace std;
 int main()
 {
+  long long x, y, z, count = 0;
+  cin >> x >> y;
+  for (int i = 0; i <= x; i++)
+  {
+    for (int j = 0; j <= x; j++)
+    {
+      z = y - i - j;
+      if (z >= 0 && z <= x)
+        count++;
+    }
+  }
+
+  cout << count << endl;
 
   return 0;
 }
